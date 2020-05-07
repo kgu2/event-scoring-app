faa104223746d4bfb0f2ab31dd86faeb517652de
Michael Teshome <35178796+michaelteshome@users.noreply.github.com>
2020-05-07 09:22:39 -0400

    Add files via upload
    Before importing Data Scraper File. Please install cheerio and request:
    
    Installation Instructions:
    " npm i cheerio request "

---
+++ src/Data-Scraping.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const cheerio = require('cheerio');
+fs = require('fs');
+
+
+function getPerformanceList() {
+    let url = 'https://va.milesplit.com/meets/385842/results/691644/raw';
+    let text = '';
+
+    request(url, (error, response, html) =>{
+        if(!error && response.statusCode == 200){
+            const $ = cheerio.load(html);
+            const content  = $('#page main #content article #meetResultsBody');
+            //console.log(content.html());
+            
+            //console.log(content.text());
+            writeToFile(content.text());
+        }
+    });
+
+}
+
+const writeToFile = (data) =>{
+    fs.writeFile('./Data.txt', data, (err) =>{
+        if(err){
+            return console.log(err);
+        }
+
+        console.log('data > ./Data.txt');
+    });
+}
+
+
+
+
+module.exports.PefromanceList = getPerformanceList();
